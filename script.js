32cdfe2d58a4e7c20eccc0de77652103d808c605
Dante <dantegaymer34@gmail.com>
2023-08-09 20:02:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("empiezo");
+setTimeout (() => console.log("me ejecuto", 5000);)
+console.log("finalizo");
+
+setTimeout (() => {
+    for (let letra of "hola") {
+        console.log(letra);
+    }
+})
